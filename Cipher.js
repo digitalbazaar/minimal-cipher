290e480e3d965b743ee26e89657a8324720d3e3b
Dave Longley <dlongley@digitalbazaar.com>
2019-07-24 14:22:01 -0400

    Add APIs for creating encrypt/decrypt streams.

--- Cipher.js
+++ Cipher.js
@@ -4,7 +4,7 @@
 'use strict';
 
 import base64url from 'base64url-universal';
-import {TextDecoder, stringToUint8Array} from './util.js';
+import {TextDecoder, TransformStream, stringToUint8Array} from './util.js';
 import {DecryptTransformer} from './DecryptTransformer.js';
 import {EncryptTransformer} from './EncryptTransformer.js';
 import * as fipsAlgorithm from './algorithms/fips.js';
@@ -43,7 +43,9 @@ export class Cipher {
   }
 
   /**
-   * Encrypts some data for one or more recipients and outputs a JWE.
+   * Creates a TransformStream that will encrypt some data for one or more
+   * recipients and output a stream of chunks, each containing an object
+   * with the property `jwe` with a JWE value.
    *
    * A list of recipients must be given in the `recipients` array, identified
    * by key agreement keys. An ephemeral ECDH key will be generated and used to
@@ -51,7 +53,55 @@ export class Cipher {
    * in the `recipients` array will be updated to include the generated
    * ephemeral ECDH key.
    *
-   * @param {Uint8Array|String} data the data to encrypt.
+   * @param {Array} recipients an array of recipients for the encrypted
+   *   content.
+   * @param {function} keyResolver a function that returns a Promise
+   *   that resolves a key ID to a DH public key.
+   * @param {number} [chunkSize=1048576] the size, in bytes, of the chunks to
+   *   break the incoming data into.
+   *
+   * @return {Promise<TransformStream>} resolves to a TransformStream.
+   */
+  async createEncryptStream({recipients, keyResolver, chunkSize}) {
+    return this._encrypt({recipients, keyResolver, chunkSize});
+  }
+
+  /**
+   * Creates a TransformStream that will decrypt one or more chunks, each one
+   * that is an object with a `jwe` property that has a JWE as a value. The
+   * stream will output chunks of Uint8Arrays consisting of the decrypted
+   * data from each chunk.
+   *
+   * The only JWEs currently supported use an `alg` of `ECDH-ES+A256KW` and
+   * `enc` of `A256GCM` or `C20P`. These parameters refer to data that has been
+   * encrypted using a 256-bit AES-GCM or ChaCha20Poly1305 content encryption
+   * key (CEK) that has been wrapped using a 256-bit AES-KW key encryption key
+   * (KEK) generated via a shared secret between an ephemeral ECDH key and a
+   * static ECDH key (ECDH-ES).
+   *
+   * @param {Object} keyAgreementKey a key agreement key API with `id` and
+   *   `deriveSecret`.
+   *
+   * @return {Promise<TransformStream>} resolves to the TransformStream.
+   */
+  async createDecryptStream({keyAgreementKey}) {
+    return new TransformStream(new DecryptTransformer({
+      keyAgreement: this.keyAgreement,
+      keyAgreementKey
+    }));
+  }
+
+  /**
+   * Encrypts some data for one or more recipients and outputs a JWE. The
+   * data to encrypt can be given as a Uint8Array or a string.
+   *
+   * A list of recipients must be given in the `recipients` array, identified
+   * by key agreement keys. An ephemeral ECDH key will be generated and used to
+   * derive shared KEKs that will wrap a randomly generated CEK. Each recipient
+   * in the `recipients` array will be updated to include the generated
+   * ephemeral ECDH key.
+   *
+   * @param {Uint8Array|String} [data] the data to encrypt.
    * @param {Array} recipients an array of recipients for the encrypted
    *   content.
    * @param {function} keyResolver a function that returns a Promise
@@ -60,6 +110,98 @@ export class Cipher {
    * @return {Promise<Object>} resolves to a JWE.
    */
   async encrypt({data, recipients, keyResolver}) {
+    if(typeof data !== Uint8Array && typeof data !== 'string') {
+      throw new TypeError('"data" must be a Uint8Array or a string.');
+    }
+    return this._encrypt({data, recipients, keyResolver});
+  }
+
+  /**
+   * Encrypts an object. The object will be serialized to JSON and passed
+   * to `encrypt`. See `encrypt` for other parameters.
+   *
+   * @param {Object} obj the object to encrypt.
+   *
+   * @return {Promise<Object>} resolves to a JWE.
+   */
+  async encryptObject({obj, ...rest}) {
+    if(typeof obj !== 'object') {
+      throw new TypeError('"obj" must be an object.');
+    }
+    return this.encrypt({data: JSON.stringify(obj), ...rest});
+  }
+
+  /**
+   * Decrypts a single JWE.
+   *
+   * The only JWEs currently supported use an `alg` of `ECDH-ES+A256KW` and
+   * `enc` of `A256GCM` or `C20P`. These parameters refer to data that has been
+   * encrypted using a 256-bit AES-GCM or ChaCha20Poly1305 content encryption
+   * key (CEK) that has been wrapped using a 256-bit AES-KW key encryption key
+   * (KEK) generated via a shared secret between an ephemeral ECDH key and a
+   * static ECDH key (ECDH-ES).
+   *
+   * @param {Object} jwe the JWE to decrypt.
+   * @param {Object} keyAgreementKey a key agreement key API with `id` and
+   *   `deriveSecret`.
+   *
+   * @return {Promise<Uint8Array|null>} resolves to the decrypted data
+   *   or `null` if the decryption failed.
+   */
+  async decrypt({jwe, keyAgreementKey}) {
+    const transformer = new DecryptTransformer({
+      keyAgreement: this.keyAgreement,
+      keyAgreementKey
+    });
+    return transformer.decrypt(jwe);
+  }
+
+  /**
+   * Decrypts a JWE that must contain an encrypted object. This method will
+   * call `decrypt` and then `JSON.parse` the resulting decrypted UTF-8 data.
+   *
+   * @param {Object} jwe the JWE to decrypt.
+   * @param {Object} keyAgreementKey a key agreement key API with `id` and
+   *   `deriveSecret`.
+   *
+   * @return {Promise<Object|null>} resolves to the decrypted object or `null`
+   *   if the decryption failed.
+   */
+  async decryptObject({jwe, keyAgreementKey}) {
+    const data = await this.decrypt({jwe, keyAgreementKey});
+    if(!data) {
+      // decryption failed
+      return null;
+    }
+    return JSON.parse(new TextDecoder().decode(data));
+  }
+
+  /**
+   * Encrypts some data for one or more recipients and outputs a JWE. The
+   * data to encrypt can be given as a Uint8Array or a string, in which
+   * case a single JWE will be output. If no data is given, a TransformStream
+   * will be returned, from which one or more chunks that are objects that
+   * include a `jwe` property with a JWE may be read after sufficient chunks,
+   * in the form of Uint8Arrays, have been written to it.
+   *
+   * A list of recipients must be given in the `recipients` array, identified
+   * by key agreement keys. An ephemeral ECDH key will be generated and used to
+   * derive shared KEKs that will wrap a randomly generated CEK. Each recipient
+   * in the `recipients` array will be updated to include the generated
+   * ephemeral ECDH key.
+   *
+   * @param {Uint8Array|String} [data] the data to encrypt.
+   * @param {Array} recipients an array of recipients for the encrypted
+   *   content.
+   * @param {function} keyResolver a function that returns a Promise
+   *   that resolves a key ID to a DH public key.
+   * @param {number} [chunkSize=1048576] the size, in bytes, of the chunks to
+   *   break the incoming data into (only applies if returning a stream).
+   *
+   * @return {Promise<Object|TransformStream>} resolves to a JWE or a
+   *   TransformStream.
+   */
+  async _encrypt({data, recipients, keyResolver, chunkSize}) {
     if(!(Array.isArray(recipients) && recipients.length > 0)) {
       throw new TypeError('"recipients" must be a non-empty array.');
     }
@@ -69,7 +211,9 @@ export class Cipher {
     if(!recipients.every(e => e.header && e.header.alg === alg)) {
       throw new Error(`All recipients must use the algorithm "${alg}".`);
     }
-    data = stringToUint8Array(data);
+    if(data) {
+      data = stringToUint8Array(data);
+    }
     const {cipher} = this;
 
     // generate a CEK for encrypting the content
@@ -106,74 +250,23 @@ export class Cipher {
     // UTF8-encoding a base64url-encoded string is the same as ASCII
     const additionalData = stringToUint8Array(encodedProtectedHeader);
 
-    // encrypt data
     const transformer = new EncryptTransformer({
       recipients,
       encodedProtectedHeader,
       cipher,
       additionalData,
-      cek
+      cek,
+      chunkSize
     });
+
+    // encrypt stream
+    if(data === undefined) {
+      return new TransformStream(new EncryptTransformer({
+        cipher, recipients, encodedProtectedHeader, cek, additionalData
+      }));
+    }
+
+    // encrypt data
     return transformer.encrypt(data);
-  }
-
-  /**
-   * Encrypts an object. The object will be serialized to JSON and passed
-   * to `encrypt`. See `encrypt` for other parameters.
-   *
-   * @param {Object} obj the object to encrypt.
-   *
-   * @return {Promise<Object>} resolves to a JWE.
-   */
-  async encryptObject({obj, ...rest}) {
-    if(typeof obj !== 'object') {
-      throw new TypeError('"obj" must be an object.');
-    }
-    return this.encrypt({data: JSON.stringify(obj), ...rest});
-  }
-
-  /**
-   * Decrypts a JWE.
-   *
-   * The only JWEs currently supported use an `alg` of `ECDH-ES+A256KW` and
-   * `enc` of `A256GCM` or `C20P`. These parameters refer to data that has been
-   * encrypted using a 256-bit AES-GCM or ChaCha20Poly1305 content encryption
-   * key (CEK) that has been wrapped using a 256-bit AES-KW key encryption key
-   * (KEK) generated via a shared secret between an ephemeral ECDH key and a
-   * static ECDH key (ECDH-ES).
-   *
-   * @param {Object} jwe the JWE to decrypt.
-   * @param {Object} keyAgreementKey a key agreement key API with `id` and
-   *   `deriveSecret`.
-   *
-   * @return {Promise<Uint8Array|null>} resolves to the decrypted data or
-   *   `null` if the decryption failed.
-   */
-  async decrypt({jwe, keyAgreementKey}) {
-    const transformer = new DecryptTransformer({
-      keyAgreement: this.keyAgreement,
-      keyAgreementKey
-    });
-    return transformer.decrypt(jwe);
-  }
-
-  /**
-   * Decrypts a JWE that must contain an encrypted object. This method will
-   * call `decrypt` and then `JSON.parse` the resulting decrypted UTF-8 data.
-   *
-   * @param {Object} jwe the JWE to decrypt.
-   * @param {Object} keyAgreementKey a key agreement key API with `id` and
-   *   `deriveSecret`.
-   *
-   * @return {Promise<Object|null>} resolves to the decrypted object or `null`
-   *   if the decryption failed.
-   */
-  async decryptObject({jwe, keyAgreementKey}) {
-    const data = await this.decrypt({jwe, keyAgreementKey});
-    if(!data) {
-      // decryption failed
-      return null;
-    }
-    return JSON.parse(new TextDecoder().decode(data));
   }
 }
