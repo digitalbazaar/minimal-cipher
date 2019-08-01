5b7d3089576716c679786e4751c80dd3cdfd3dd7
andrewjones <ajones@digitalbazaar.com>
2019-08-01 18:22:41 -0400

    Add KaK and decrypt tests for Objects.

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const {Cipher} = require('../../index');
-const base58 = require('../base58esm');
+const KaK = require('../KaK');
 const chaiCipher = require('../chai-cipher');
 chai.should();
 chai.use(chaiCipher);
@@ -12,22 +12,22 @@ describe('minimal-cipher should use ', function() {
     describe(`the ${algorithm} algorithm`, function() {
 
       // each test inits data to null
-      let cipher, keyPair, publicKeyNode, recipients = null;
+      let cipher, keyAgreementKey, publicKeyNode, recipients = null;
 
       // keyResolver returns publicKeyNode
       const keyResolver = async () => publicKeyNode;
 
       beforeEach(async function() {
         cipher = new Cipher({version: algorithm});
-        keyPair = await cipher.keyAgreement.deriveEphemeralKeyPair();
+        keyAgreementKey = new KaK();
         publicKeyNode = {
           '@context': 'https://w3id.org/security/v2',
-          id: 'urn:123',
-          type: 'X25519KeyAgreementKey2019',
-          publicKeyBase58: base58.encode(keyPair.publicKey)
+          id: keyAgreementKey.id,
+          type: keyAgreementKey.type,
+          publicKeyBase58: keyAgreementKey.publicKeyBase58
         };
         recipients = [{
-          header: {kid: publicKeyNode.id, alg: 'ECDH-ES+A256KW'}
+          header: {kid: keyAgreementKey.id, alg: 'ECDH-ES+A256KW'}
         }];
       });
 
@@ -50,8 +50,30 @@ describe('minimal-cipher should use ', function() {
         result.should.be.a.JWE;
       });
 
-      it.skip('to encrypt a stream', async function() {
+      it('to decrypt a simple Uint8Array', async function() {
+        const data = new Uint8Array([0x01, 0x02, 0x03]);
+        const jwe = await cipher.encrypt({data, recipients, keyResolver});
+        jwe.should.be.a.JWE;
+        const result = await cipher.decrypt({jwe, keyAgreementKey});
+        result.should.deep.equal(data);
+      });
 
+      it('to decrypt a simple string', async function() {
+        const data = 'simple';
+        const jwe = await cipher.encrypt({data, recipients, keyResolver});
+        jwe.should.be.a.JWE;
+        const result = await cipher.decrypt({jwe, keyAgreementKey});
+        const resultString = new TextDecoder('utf-8').decode(result);
+        resultString.should.deep.equal(data);
+      });
+
+      it('to decrypt a simple object', async function() {
+        const obj = {simple: true};
+        const jwe = await cipher.encryptObject(
+          {obj, recipients, keyResolver});
+        jwe.should.be.a.JWE;
+        const result = await cipher.decryptObject({jwe, keyAgreementKey});
+        result.should.deep.equal(obj);
       });
 
     });
