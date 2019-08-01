96a82bf1bdb966bc2497c3593035115a4738c190
andrewjones <ajones@digitalbazaar.com>
2019-08-01 16:53:30 -0400

    Add chai assertion for JWE.
    Skip encrypt stream test.

---
+++ test/chai-cipher.js
@@ -0,0 +1,22 @@
+module.exports = function(chai) {
+  const {Assertion} = chai;
+  Assertion.addProperty('JWE', function() {
+    const jwe = this._obj;
+    new Assertion(jwe).to.be.an('object');
+    new Assertion(jwe.protected).to.exist;
+    new Assertion(jwe.protected).to.be.a(
+      'string', 'Expected the property protected to be a string.');
+    new Assertion(jwe.recipients).to.exist;
+    new Assertion(jwe.recipients).to.be.an(
+      'array', 'Expected JWE recipients to be an array.');
+    new Assertion(jwe.iv).to.exist;
+    new Assertion(jwe.iv).to.be.a(
+      'string', 'Expected JWE Initialization Vector to be a string.');
+    new Assertion(jwe.ciphertext).to.exist;
+    new Assertion(jwe.ciphertext).to.be.a(
+      'string', 'Expected JWE ciphertext to be a string.');
+    new Assertion(jwe.tag).to.exist;
+    new Assertion(jwe.tag).to.be.a(
+      'string', 'Expected JWE tag to be a string');
+  });
+};
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,13 +1,22 @@
+const chai = require('chai');
 const {Cipher} = require('../../index');
 const base58 = require('../base58esm');
+const chaiCipher = require('../chai-cipher');
+chai.should();
+chai.use(chaiCipher);
 
 const cipherAlgorithms = ['recommended', 'fips'];
 
 describe('minimal-cipher should use ', function() {
   cipherAlgorithms.forEach(algorithm => {
     describe(`the ${algorithm} algorithm`, function() {
-      let cipher, keyPair, publicKeyNode = null;
-      const keyResolver = async () => publicKeyNode
+
+      // each test inits data to null
+      let cipher, keyPair, publicKeyNode, recipients = null;
+
+      // keyResolver returns publicKeyNode
+      const keyResolver = async () => publicKeyNode;
+
       beforeEach(async function() {
         cipher = new Cipher({version: algorithm});
         keyPair = await cipher.keyAgreement.deriveEphemeralKeyPair();
@@ -17,18 +26,27 @@ describe('minimal-cipher should use ', function() {
           type: 'X25519KeyAgreementKey2019',
           publicKeyBase58: base58.encode(keyPair.publicKey)
         };
-      });
-      it('to encrypt some simple bites', async function() {
-        const data = new Uint8Array([0x01, 0x02, 0x03]);
-        const recipients = [{
-          header: {kid: publicKeyNode.id}
+        recipients = [{
+          header: {kid: publicKeyNode.id, alg: 'ECDH-ES+A256KW'}
         }];
-        const result = await cipher.encrypt({data, recipients, keyResolver});
-        console.log(result);
       });
-      it('to encrypt a stream', async function() {
+
+      it('to encrypt a simple Uint8Array', async function() {
+        const data = new Uint8Array([0x01, 0x02, 0x03]);
+        const result = await cipher.encrypt({data, recipients, keyResolver});
+        result.should.be.a.JWE;
+      });
+
+      it('to encrypt a simple string', async function() {
+        const data = 'simple';
+        const result = await cipher.encrypt({data, recipients, keyResolver});
+        result.should.be.a.JWE;
+      });
+
+      it.skip('to encrypt a stream', async function() {
 
       });
+
     });
   });
 });
