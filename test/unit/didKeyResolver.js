740499ac9422c152a3f6df1ccf0dbd409c522fa6
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-07-16 09:48:57 -0400

    Move test deps to devDeps.

--- test/unit/didKeyResolver.js
+++ test/unit/didKeyResolver.js
@@ -1,12 +1,15 @@
 /*!
  * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
  */
+'use strict';
+
 const {CachedResolver} = require('@digitalbazaar/did-io');
-const {driver} = require('@digitalbazaar/did-method-key');
+const {driver: didKeyDriver} = require('@digitalbazaar/did-method-key');
+
 const resolver = new CachedResolver();
 
 // Config did-io to support did:key driver
-resolver.use(driver());
+resolver.use(didKeyDriver());
 
 function createKeyResolver() {
   return async function keyResolver({id} = {}) {
