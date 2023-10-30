72e4438a97ab6fd932041218275a6917fc1f2bf9
Dave Longley <dlongley@digitalbazaar.com>
2023-10-30 12:14:59 -0400

    Use latest did-method-key driver package.

--- test/unit/didKeyResolver.js
+++ test/unit/didKeyResolver.js
@@ -1,13 +1,20 @@
 /*!
- * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Digital Bazaar, Inc. All rights reserved.
  */
 import {CachedResolver} from '@digitalbazaar/did-io';
-import {driver as didKeyDriver} from '@digitalbazaar/did-method-key';
+import {driver} from '@digitalbazaar/did-method-key';
+import {Ed25519VerificationKey2020} from
+  '@digitalbazaar/ed25519-verification-key-2020';
 
 const resolver = new CachedResolver();
 
-// Config did-io to support did:key driver
-resolver.use(didKeyDriver());
+// config did-io to support did:key driver
+const didKeyDriver = driver();
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'z6Mk',
+  fromMultibase: Ed25519VerificationKey2020.from
+});
+resolver.use(didKeyDriver);
 
 export function createKeyResolver() {
   return async function keyResolver({id} = {}) {
