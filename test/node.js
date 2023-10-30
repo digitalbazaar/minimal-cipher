1a0516736102cef5be5c7b9f7028aaf433ba6a5e
Dave Longley <dlongley@digitalbazaar.com>
2023-10-30 12:27:42 -0400

    Require node >= 18.

--- test/node.js
+++ test/node.js
@@ -1,11 +1,3 @@
-// WebCrypto polyfill if needed
-import crypto from 'node:crypto';
-import webcrypto from 'isomorphic-webcrypto';
-
-if(!crypto.webcrypto) {
-  crypto.webcrypto = webcrypto;
-}
-
 // WebStreams polyfill if needed
 import {
   ReadableStream, TransformStream
