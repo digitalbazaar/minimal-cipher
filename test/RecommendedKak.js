625aac3cf64c9165d3b1e935b4cac9a547a2e84d
Dave Longley <dlongley@digitalbazaar.com>
2023-11-04 15:08:00 -0400

    Refactor test Kak API.

--- test/RecommendedKak.js
+++ test/RecommendedKak.js
@@ -12,31 +12,37 @@ import nacl from 'tweetnacl';
 import {store} from './store.js';
 
 export class RecommendedKak {
-  constructor({keyPair, id = 'urn:123'} = {}) {
+  constructor({id, keyPair} = {}) {
     this.id = id;
     this.type = 'X25519KeyAgreementKey2020';
-    if(!keyPair) {
+    this.privateKey = keyPair.secretKey || keyPair.privateKey;
+    this.publicKey = keyPair.publicKey;
+    this.publicKeyMultibase = keyPair.publicKeyMultibase ||
+      multibaseEncode(MULTICODEC_X25519_PUB_HEADER, this.publicKey);
+  }
+
+  static async generate({id = 'urn:123', legacyKeyPair} = {}) {
+    let keyPair;
+    if(legacyKeyPair) {
+      keyPair = {
+        privateKey: base58.decode(legacyKeyPair.privateKeyBase58),
+        publicKey: base58.decode(legacyKeyPair.publicKeyBase58)
+      };
+    } else {
       // use `tweetnacl` lib to cross-compare X25519 implementations
       keyPair = nacl.box.keyPair();
-      this.privateKey = keyPair.secretKey;
-      this.publicKey = keyPair.publicKey;
-    } else {
-      this.privateKey = base58.decode(keyPair.privateKeyBase58);
-      this.publicKey = base58.decode(keyPair.publicKeyBase58);
     }
-    this.publicKeyMultibase = multibaseEncode(
-      MULTICODEC_X25519_PUB_HEADER, this.publicKey
-    );
-
-    store.set(id, this.publicKeyNode);
+    const kak = new RecommendedKak({id, keyPair});
+    store.set(id, kak.export());
+    return kak;
   }
+
   /**
-   * Formats this KaK into an object
-   * complaint with JSON-LD-Signatures.
+   * Formats this KaK into an object.
    *
    * @returns {object} A JSON-LD object.
    */
-  get publicKeyNode() {
+  export() {
     return {
       '@context': 'https://w3id.org/security/suites/x25519-2020/v1',
       id: this.id,
@@ -44,6 +50,7 @@ export class RecommendedKak {
       publicKeyMultibase: this.publicKeyMultibase
     };
   }
+
   /**
    * Formats this Kak into a partially complete JOSE Header
    * that can be used as a recipient of a JWE.
@@ -55,6 +62,7 @@ export class RecommendedKak {
       header: {kid: this.id, alg: 'ECDH-ES+A256KW'}
     };
   }
+
   async deriveSecret({publicKey}) {
     const remotePublicKey = multibaseDecode(
       MULTICODEC_X25519_PUB_HEADER, publicKey.publicKeyMultibase);
